8e10e9c6c35607da73385ab5a7a02e89bf9e56d0
Mathew Payne <geekmasher@gmail.com>
2023-08-14 19:06:22 +0100

    feat: install script and action

---
+++ scripts/install-extractor.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+EXTRACTOR_NAME="${1}"
+EXTRACTOR_LOCATIONS="$HOME/.codeql/extractors"
+
+if [ -z "$EXTRACTOR_NAME" ]; then
+    echo "No extractor name provided"
+    exit 1
+fi
+
+mkdir -p $EXTRACTOR_LOCATIONS
+
+gh release list -L 1 -R "advanced-security/codeql-extractor-$EXTRACTOR_NAME"
+
+gh release download \
+    -R "advanced-security/codeql-extractor-$EXTRACTOR_NAME" \
+    -D "$EXTRACTOR_LOCATIONS" \
+    --clobber \
+    --pattern 'extractor-*.tar.gz'
+
+tar -zxf "$EXTRACTOR_LOCATIONS/extractor-$EXTRACTOR_NAME.tar.gz" --directory "$EXTRACTOR_LOCATIONS"
