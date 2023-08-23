cd07c050eef88e88320f9c23d24b1cfdabf89d75
Mathew Payne <geekmasher@gmail.com>
2023-08-23 14:41:40 +0100

    Add run tests script

---
+++ scripts/run-tests.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if which codeql >/dev/null; then
+  CODEQL_BINARY="codeql"
+elif gh codeql >/dev/null; then
+  CODEQL_BINARY="gh codeql"
+else
+  gh extension install github/gh-codeql
+  CODEQL_BINARY="gh codeql"
+fi
+
+$CODEQL_BINARY test run ql/test
