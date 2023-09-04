70c34de008c5439cc7faa11068c1e76171323acb
Mathew Payne <geekmasher@gmail.com>
2023-09-04 10:36:54 +0100

    Remove JSON from extractor

--- tools/pre-finalize.sh
+++ tools/pre-finalize.sh
@@ -5,6 +5,9 @@ set -eu
 "$CODEQL_DIST/codeql" database index-files \
     "--include=**/*.yml" \
     "--include=**/*.yaml" \
+    "--include=**/*.json" \
+    "--include=**/*.jsonc" \
+    "--include=**/*.jsonl" \
     --size-limit=5m \
     --language yaml \
     -- \
