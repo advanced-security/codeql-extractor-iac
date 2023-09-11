22b30c0580c857252578c9c655e773568a225a24
Mathew Payne <geekmasher@gmail.com>
2023-09-11 18:15:05 +0100

    Update publish script

--- scripts/publish-extractor-pack.sh
+++ scripts/publish-extractor-pack.sh
@@ -5,13 +5,17 @@ EXTRACTOR_INFO="./codeql-extractor.yml"
 EXTRACTOR_NAME="iac"
 EXTRACTOR_VERSION=$(grep version $EXTRACTOR_INFO | awk '{print $2}')
 
-CURRENT_RELEASE=$(gh release list | head -n 1 | awk '{print $1}')
+LATEST_RELEASE=$(gh release list | head -n 1 | awk '{print $1}' | sed 's/v//')
 
 echo "[+] ${EXTRACTOR_NAME} (${EXTRACTOR_VERSION})"
-echo "[+] Last release: ${CURRENT_RELEASE}"
+echo "[+] Last release: ${LATEST_RELEASE}"
 
-if [ "$CURRENT_RELEASE" != "$EXTRACTOR_VERSION" ]; then
+if [ "$LATEST_RELEASE" != "$EXTRACTOR_VERSION" ]; then
     echo "[+] New Extractor version being released"
+
+    # create extractor pack
+    ./scripts/create-extractor-pack.sh
+
     # bundle extractor
     tar czf extractor-$EXTRACTOR_NAME.tar.gz extractor-pack
 
@@ -22,6 +26,6 @@ if [ "$CURRENT_RELEASE" != "$EXTRACTOR_VERSION" ]; then
         --notes "$EXTRACTOR_NAME Extractor v$EXTRACTOR_VERSION" \
         extractor-$EXTRACTOR_NAME.tar.gz
 
-then
+else
     echo "[+] Extractor is up to date"
 fi
