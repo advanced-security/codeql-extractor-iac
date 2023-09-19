2c5b76d8a899521bd416908081056f086a824363
Mathew Payne <geekmasher@gmail.com>
2023-09-19 20:36:03 +0100

    Fix extractor version issue

--- .github/action/dist/index.js
+++ .github/action/dist/index.js
@@ -13530,9 +13530,9 @@ const toolcache = __importStar(__nccwpck_require__(7784));
 const github = __importStar(__nccwpck_require__(5438));
 const toolrunner = __importStar(__nccwpck_require__(8159));
 exports.EXTRACTOR_REPOSITORY = "advanced-security/codeql-extractor-iac";
-exports.EXTRACTOR_VERSION = "v0.0.3";
+exports.EXTRACTOR_VERSION = "v0.0.3"; // stable version
 async function newCodeQL() {
-    var version = core.getInput("codeql-version");
+    var version = core.getInput("extractor-version");
     if (version === "") {
         version = exports.EXTRACTOR_VERSION;
     }
@@ -13672,6 +13672,7 @@ async function codeqlDatabaseAnalyze(codeql, database_path) {
         "analyze",
         "--format",
         "sarif-latest",
+        "--sarif-add-query-help",
         "--output",
         codeql_output,
     ];
