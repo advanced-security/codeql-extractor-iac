1c1aeaccd3d9cb0767d11840c319cff51f3035fd
Mathew Payne <geekmasher@gmail.com>
2023-10-05 18:58:42 +0100

    Add GCP tests

---
+++ ql/test/library-tests/hcl/gcp/storage.tf
@@ -0,0 +1,10 @@
+resource "google_storage_bucket_access_control" "public_rule" {
+  bucket = google_storage_bucket.bucket.name
+  role   = "READER"
+  entity = "allUsers"
+}
+
+resource "google_storage_bucket" "bucket" {
+  name     = "static-content-bucket"
+  location = "US"
+}
